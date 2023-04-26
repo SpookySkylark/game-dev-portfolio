cf4a88d5740580d24572e2ee3f6df903bb1a1dd0
SpookySkylark <64929944+SpookySkylark@users.noreply.github.com>
2023-04-25 17:46:16 -0700

    description and title

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Skylar Kramer Dev Blog',
+    description: 'I\'m going to make games and you can\'t stop me.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
