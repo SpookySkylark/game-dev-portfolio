d261424a9001a7378eafcbd38446c3f058799fca
SpookySkylark <64929944+SpookySkylark@users.noreply.github.com>
2023-04-26 13:04:58 -0700

    testing embed methods

--- pages/posts/spacegame_js.js
+++ pages/posts/spacegame_js.js
@@ -1,15 +1,6 @@
----
-title: Space Trading Game
-date: 2023/4/26
-description: Exploring my space trading game.
-tag: app development
-author: You
----
 
-```
-export default function Nextra({ Component, pageProps }) {
+function SpaceTradeGame()  {
   return `<div>
             <iframe src="https://game-dev-portfolio-pi.vercel.app/apps/space/index.html" style="border:0px #000000 none;" name="Game name" scrolling="no" frameborder="1" marginheight="px" marginwidth="320px" height="320px" width="480px"></iframe>
-        </div>`
+        </div>`;
 }
-```
